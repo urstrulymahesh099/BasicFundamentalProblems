b282db2935eb4d47f0d260657a2b410a6ebddd8b
mago mahi <maheshhonnamurthy099@gmail.com>
2022-11-06 21:57:59 +0530

    [Mahesh]Add:EvenOdd

---
+++ BasicFundamentalProblems/EvenOdd.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicFundamentalProblems
+{
+    public class EvenOdd
+    {
+        public void Number()
+        {
+            Console.WriteLine("Enter Value");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num % 2 == 0)
+                Console.WriteLine("Number is even number");
+            else
+                Console.WriteLine("Number is odd number");
+        }
+
+    }
+}
