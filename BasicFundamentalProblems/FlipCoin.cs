a969fdde6d22804026c7c7206fcd0794e3383ad4
mago mahi <maheshhonnamurthy099@gmail.com>
2022-11-06 20:17:43 +0530

    [Mahesh]Add: FlipCoin Structure is Created

---
+++ BasicFundamentalProblems/FlipCoin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace BasicFundamentalProblems
+
+{
+
+    public class FlipCoin
+    {
+        Random random = new Random();
+        public void Play()
+        {
+            double head = 0, tail = 0;
+            Console.WriteLine("Number of times of flip the coin");        
+            int n = Convert.ToInt32(Console.ReadLine());
+            double COUNT = n;
+            while (n > 0)
+            {
+                if (random.NextDouble() > 0.5)
+                    head++;
+                else
+                    tail++;
+                n--;
+            }
+            Console.WriteLine("Head will come - " +  head);
+            Console.WriteLine("Tail will come - " +  tail);
+            double HEAD_PERCENTAGE = (head / COUNT) * 100;
+            double TAIL_PERCENTAGE = (tail / COUNT) * 100;
+            Console.WriteLine("Head Percenage will be - " + HEAD_PERCENTAGE + "\n" + "Tail Percentage will be - " + TAIL_PERCENTAGE);
+        }
+    }
+}
---
+++ BasicFundamentalProblems/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace BasicFundamentalProblems
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("1. FlipCoin" + "           " + "2. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        FlipCoin enter = new FlipCoin();
+                        enter.Play();
+                        break;
+                    case 2:
+                        flag = false;
+                        Console.WriteLine("Try Again");
+                        break;
+                }
+            }
+        }
+    }
+}
