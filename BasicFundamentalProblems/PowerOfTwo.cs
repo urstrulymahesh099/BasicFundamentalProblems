0e1bdba4f2f471563416af8697e38550cc7b5cc3
mago mahi <maheshhonnamurthy099@gmail.com>
2022-11-06 21:19:52 +0530

    [Mahesh]Add: Powe of Two

---
+++ BasicFundamentalProblems/PowerOfTwo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicFundamentalProblems
+{
+    public class PowerOfTwo
+    {
+        public void Table()
+        {
+            Console.WriteLine("Enter the value ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < N && N < 32; i++)
+            {
+                double number = Math.Pow(2, i);
+                Console.WriteLine("2^" + i + " = " + number);
+            }
+
+        }
+    }
+}
--- BasicFundamentalProblems/Program.cs
+++ BasicFundamentalProblems/Program.cs
@@ -9,9 +9,10 @@ namespace BasicFundamentalProblems
             while (flag)
             {
                 Console.WriteLine("1. FlipCoin" + "\n" + "2. Leap Year " + "\n" + "3. Exit");
-                Console.WriteLine("Enter The Above Mention Case");
-                int Case = Convert.ToInt32(Console.ReadLine());
-                switch (Case)
+                Console.WriteLine("1. FlipCoin" + "\n" + "2. Leap Year " + "\n" + "3. Power Of Two" + "\n" + "4. Exit");
+                Console.WriteLine("Enter The Above Mention Option");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
                 {
                     case 1:
                         FlipCoin enter = new FlipCoin();
@@ -22,6 +23,10 @@ namespace BasicFundamentalProblems
                         leap.Year();
                         break;
                     case 3:
+                        PowerOfTwo power = new PowerOfTwo();
+                        power.Table();
+                        break;
+                    case 4:
                         flag = false;
                         Console.WriteLine("Try Again");
                         break;
