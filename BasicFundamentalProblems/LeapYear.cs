e965adfb20733055707ba5f3413de74ea68747c0
mago mahi <maheshhonnamurthy099@gmail.com>
2022-11-06 20:34:43 +0530

    [Mahesh]Add: Leap Year

---
+++ BasicFundamentalProblems/LeapYear.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicFundamentalProblems
+{
+    public class LeapYear
+    {
+        public void Year()
+        {
+            Console.WriteLine("Enter the Year");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if ((year % 4 == 0 || year % 400 == 0) && year % 100 != 0)
+                Console.WriteLine("Year " + year + " is Leap Year");
+            else
+                Console.WriteLine("Year is not a Leap Year");
+        }
+    }
+}
--- BasicFundamentalProblems/Program.cs
+++ BasicFundamentalProblems/Program.cs
@@ -8,15 +8,20 @@ namespace BasicFundamentalProblems
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("1. FlipCoin" + "           " + "2. Exit");
-                int option = Convert.ToInt32(Console.ReadLine());
-                switch (option)
+                Console.WriteLine("1. FlipCoin" + "\n" + "2. Leap Year " + "\n" + "3. Exit");
+                Console.WriteLine("Enter The Above Mention Case");
+                int Case = Convert.ToInt32(Console.ReadLine());
+                switch (Case)
                 {
                     case 1:
                         FlipCoin enter = new FlipCoin();
                         enter.Play();
                         break;
                     case 2:
+                        LeapYear leap = new LeapYear();
+                        leap.Year();
+                        break;
+                    case 3:
                         flag = false;
                         Console.WriteLine("Try Again");
                         break;
