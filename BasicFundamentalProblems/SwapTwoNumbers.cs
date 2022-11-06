ca1ab683d7c0fa790197e800007de736123d2029
mago mahi <maheshhonnamurthy099@gmail.com>
2022-11-06 21:50:32 +0530

    [Mahesh]Add:SwapTwoNumbers

---
+++ BasicFundamentalProblems/SwapTwoNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicFundamentalProblems
+{
+    public class SwapTwoNumbers
+    {
+        public void Swap()
+        {
+            Console.WriteLine("Enter First Number = ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Second Number = ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+
+            Console.WriteLine("After Swap" + "\n" + "First Number Is " + a + "\n" + "Second Number Is " + b);
+        }
+    }
+}
