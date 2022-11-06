52d9319200bbf8b9efe1b89073c9d7348a78f60d
mago mahi <maheshhonnamurthy099@gmail.com>
2022-11-06 21:41:18 +0530

    [Mahesh]Add: QuoteintAndReminder

--- BasicFundamentalProblems/Program.cs
+++ BasicFundamentalProblems/Program.cs
@@ -7,34 +7,39 @@ namespace BasicFundamentalProblems
         {
             bool flag = true;
             while (flag)
-            {
-                Console.WriteLine("1. FlipCoin" + "\n" + "2. Leap Year " + "\n" + "3. Power Of Two" + "\n" + "4. Harmonic Number" + "\n" + "5. Exit");
-                Console.WriteLine("Enter The Above Mention Option");
-                int option = Convert.ToInt32(Console.ReadLine());
-                switch (option)
                 {
-                    case 1:
-                        FlipCoin enter = new FlipCoin();
-                        enter.Play();
-                        break;
-                    case 2:
-                        LeapYear leap = new LeapYear();
-                        leap.Year();
-                        break;
-                    case 3:
-                        PowerOfTwo power = new PowerOfTwo();
-                        power.Table();
-                        break;
-                    case 4:
-                        HarmonicNumber number = new HarmonicNumber();
-                        number.Harmonic();
-                        break;
-                    case 5:
-                        flag = false;
-                        Console.WriteLine("Try Again");
-                        break;
+                    Console.WriteLine("1. FlipCoin" + "\n" + "2. Leap Year " + "\n" + "3. Power Of Two" + "\n" + "4. Harmonic Number" + "\n" + "5. Find Quoteint and Reminder" + "\n" + "6. Exit");
+                    Console.WriteLine("Enter The Above Mention Option");
+                    int option = Convert.ToInt32(Console.ReadLine());
+                    switch (option)
+                    {
+                        case 1:
+                            FlipCoin enter = new FlipCoin();
+                            enter.Play();
+                            break;
+                        case 2:
+                            LeapYear leap = new LeapYear();
+                            leap.Year();
+                            break;
+                        case 3:
+                            PowerOfTwo power = new PowerOfTwo();
+                            power.Table();
+                            break;
+                        case 4:
+                            HarmonicNumber number = new HarmonicNumber();
+                            number.Harmonic();
+                            break;
+                        case 5:
+                            QuoteintAndReminder quo = new QuoteintAndReminder();
+                            quo.Quotient();
+                            break;
+                        case 6:
+                            flag = false;
+                            Console.WriteLine("Try Again");
+                            break;
+                    }
                 }
             }
         }
     }
-}
+
---
+++ BasicFundamentalProblems/QuoteintAndReminder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicFundamentalProblems
+{
+    public class QuoteintAndReminder
+    {
+        public void Quotient()
+        {
+            Console.WriteLine("Enter Dividend");
+            int dividend = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Divisor");
+            int divisor = Convert.ToInt32(Console.ReadLine());
+            int quotient = (dividend / divisor);
+            int reminder = (dividend % divisor);
+            Console.WriteLine("Quotient is  " + quotient);
+            Console.WriteLine("Remainder is " + reminder);
+        }
+    }
+}
