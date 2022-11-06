f7637a69ef02306416728df4cae6df266092177a
mago mahi <maheshhonnamurthy099@gmail.com>
2022-11-06 22:05:22 +0530

    [Mahesh]Add:VowelConsonant

--- BasicFundamentalProblems/Program.cs
+++ BasicFundamentalProblems/Program.cs
@@ -7,39 +7,51 @@ namespace BasicFundamentalProblems
         {
             bool flag = true;
             while (flag)
+            {
+                Console.WriteLine("1. FlipCoin" + "\n" + "2. Leap Year " + "\n" + "3. Power Of Two" + "\n" + "4. Harmonic Number" + "\n" + "5. Find Quoteint and Reminder" + "\n" + "6. Swapping Two Numbers" + "\n" + "7. Find Even or Odd Number" + "\n" + "8. Exit");
+                Console.WriteLine("1. FlipCoin" + "\n" + "2. Leap Year " + "\n" + "3. Power Of Two" + "\n" + "4. Harmonic Number" + "\n" + "5. Find Quoteint and Reminder" + "\n" + "6. Swapping Two Numbers" + "\n" + "7. Find Even or Odd Number" + "\n" + "8.Find Alphabets - Vowel Consonant" + "\n" + "9. Exit");
+                Console.WriteLine("Enter The Above Mention Option");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
                 {
-                    Console.WriteLine("1. FlipCoin" + "\n" + "2. Leap Year " + "\n" + "3. Power Of Two" + "\n" + "4. Harmonic Number" + "\n" + "5. Find Quoteint and Reminder" + "\n" + "6. Exit");
-                    Console.WriteLine("Enter The Above Mention Option");
-                    int option = Convert.ToInt32(Console.ReadLine());
-                    switch (option)
-                    {
-                        case 1:
-                            FlipCoin enter = new FlipCoin();
-                            enter.Play();
-                            break;
-                        case 2:
-                            LeapYear leap = new LeapYear();
-                            leap.Year();
-                            break;
-                        case 3:
-                            PowerOfTwo power = new PowerOfTwo();
-                            power.Table();
-                            break;
-                        case 4:
-                            HarmonicNumber number = new HarmonicNumber();
-                            number.Harmonic();
-                            break;
-                        case 5:
-                            QuoteintAndReminder quo = new QuoteintAndReminder();
-                            quo.Quotient();
-                            break;
-                        case 6:
-                            flag = false;
-                            Console.WriteLine("Try Again");
-                            break;
-                    }
+                    case 1:
+                        FlipCoin enter = new FlipCoin();
+                        enter.Play();
+                        break;
+                    case 2:
+                        LeapYear leap = new LeapYear();
+                        leap.Year();
+                        break;
+                    case 3:
+                        PowerOfTwo power = new PowerOfTwo();
+                        power.Table();
+                        break;
+                    case 4:
+                        HarmonicNumber number = new HarmonicNumber();
+                        number.Harmonic();
+                        break;
+                    case 5:
+                        QuoteintAndReminder quo = new QuoteintAndReminder();
+                        quo.Quotient();
+                        break;
+                    case 6:
+                        SwapTwoNumbers swa = new SwapTwoNumbers();
+                        swa.Swap();
+                        break;
+                    case 7:
+                        EvenOdd eve = new EvenOdd();
+                        eve.Number();
+                        break;
+                    case 8:
+                        VowelConsonant vow = new VowelConsonant();
+                        vow.Vowels();
+                        break;
+                    case 9:
+                        flag = false;
+                        Console.WriteLine("Try Again");
+                        break;
                 }
             }
         }
     }
-
+}
---
+++ BasicFundamentalProblems/VowelConsonant.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicFundamentalProblems
+{
+    public class VowelConsonant
+    {
+        public void Vowels()
+        {
+            Console.Write("Input an Alphabet: ");
+            char ch = Convert.ToChar(Console.ReadLine().ToLower());
+
+            switch (ch)
+
+            {
+                case 'a':
+                    Console.WriteLine("Alphabet is vowel");
+                    break;
+                case 'i':
+                    Console.WriteLine("Alphabet is vowel");
+                    break;
+                case 'o':
+                    Console.WriteLine("Alphabet is vowel");
+                    break;
+                case 'u':
+                    Console.WriteLine("Alphabet is vowel");
+                    break;
+                case 'e':
+                    Console.WriteLine("Alphabet is vowel");
+                    break;
+                default:
+                    Console.WriteLine("Alphabet is consonant");
+                    break;
+            }
+        }
+
+    }
+}
